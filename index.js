e59d9c63b5b64a113eda107da78b89cb0e942ee8
ckesurf <ckesurf@gmail.com>
2021-10-13 17:22:27 -0400

    commit tree bbc5d221bbb1e244c053e8395be57ebae59a0992

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers() — Declare the variable returnFirstTwoDrivers with const and assign an anonymous function to it. The assigned function should accept an array of drivers as an argument and return the first two drivers in the array.
+
+const returnFirstTwoDrivers = function(drivers) {
+    // return first two drivers
+    // drivers = ['Chris', 'Bob', 'Freddy', 'Claudia']
+    return [drivers[0], drivers[1]]
+};
+
+// returnLastTwoDrivers() — Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the last two drivers in the array.
+
+const returnLastTwoDrivers = (drivers) => {
+    // return [drivers[2], drivers[3]]
+    // ['Sally', 'Bob', 'Freddy', 'Claudia']
+    // last one: drivers[drivers.length - 1]
+    // second to last one: drivers[drivers.length - 2]
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+}
+
+const selectDifferentDrivers = (drivers, returnLastTwoDrivers) => {
+    return returnLastTwoDrivers(drivers)
+}
